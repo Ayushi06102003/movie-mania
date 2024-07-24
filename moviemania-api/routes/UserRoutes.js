89940b622afc15f95ffbadcd4b3e4ac82599f896
Ayushi06102003 <ayushisingh003366@gmail.com>
2024-07-24 09:55:06 +0530

    "Updated UserRoutes.js to define routes for user-related API endpoints and refactored server.js to set up Express.js server, connect to MongoDB database, and define routes."

--- moviemania-api/routes/UserRoutes.js
+++ moviemania-api/routes/UserRoutes.js
@@ -1,12 +1,21 @@
+
+//imports three functions from the UserController module:
+
 const {
   addToLikedMovies,
   getLikedMovies,
   removeFromLikedMovies,
 } = require("../cotrollers/UserController");
 
+//Creating an Express router
 const router = require("express").Router();
 
+
+//Defining routes
 router.post("/add", addToLikedMovies);
 router.get("/liked/:email", getLikedMovies);
 router.put("/delete",removeFromLikedMovies);
+
+
+//Exporting the router
 module.exports = router;
--- moviemania-api/server.js
+++ moviemania-api/server.js
@@ -1,19 +1,29 @@
+//Importing Dependencies
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/UserRoutes");
 
+//Setting up the Express.js Server
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
+
+//Connecting to the MongoDB Database
 mongoose.connect("mongodb://127.0.0.1:27017/netflix").then(() => {
   console.log("DB Connected");
 });
 
+
+//Defining Routes
 app.use("/api/user", userRoutes);
 
+
+//Starting the Server
 app.listen(5000, () => {
   console.log("Backend server is running");
 });
+
+
+//sets up an Express.js server that connects to a MongoDB database, defines routes for user-related API endpoints, and starts the server on port 5000.
